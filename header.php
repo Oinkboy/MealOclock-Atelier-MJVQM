ef1c3f9c88d7fc0a8261bedbdf535192cd2617ae
Max'-C <maxime.cohier@gmail.com>
2018-07-13 14:56:13 +0200

    Modif css

---
+++ header.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+      <link rel="stylesheet" href="public/assets/css/style.css">
+      <link href="https://fonts.googleapis.com/css?family=Miriam+Libre:400,700" rel="stylesheet">
+
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+      <title>MealOclock</title>
+    </head>
+
+<body>
+    <div class="container-fluid">
+    <header class="mb-4">
+            <div id="header-band"></div>
+            <nav class="navbar navbar-light justify-content-center">
+                  <div class="navbar-collapse">
+                        <ul class="nav nav-pills d-flex">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                              <li class="nav-item flex-fill p-2 form-li">
+                                    <form class="form-inline">
+                                          <i class="fas fa-search ml-3"></i>
+                                          <input class="form" type="search" placeholder="" aria-label="Search">
+                                    </form>
+                              </li>
+                              <li class="nav-item p-2 flex-fill">
+                                    <a class="h-100" href="#">
+                                          <img src="public/assets/images/E03-logo.svg" alt="">
+                                    </a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link d-inline sub" href="#"><i class="fas fa-sign-in-alt "></i>Connexion</a>
+
+                                    <a class="nav-link d-inline sub" href="#"><i class="fas fa-edit"></i>Inscription</a>
+                              </li>
+
+                        </ul>
+
+                  </div>
+            </nav>
+            <nav class="collapse navbar-collapse navbar navbar-expand-md navbar-light justify-content-center" id="navbarTogglerDemo02">
+                  <!-- Cette partie va automatique être masquée en version mobile -->
+                  <div class="" >
+                        <ul class="navbar-nav">
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Communautés</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Evénements</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Membres</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Forum</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">&Agrave; table</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Blog</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a>
+                              </li>
+
+                        </ul>
+                  </div>
+            </nav>
+
+      </header>
+
+
+
+        <div class="text" id="table">
+            <h1>A table</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+    <main class="">
+        <div class="row test">
+            <div class="col-lg-3 img-container">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>
+            <div class="text col-lg">
+                <h1>Un bon repas ça se partage !</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+        <div class="row test">
+            <div class="img-container col-lg-3">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>
+            <div class="text col-lg">
+                <h1>Un bon repas ça se partage !</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+         <div class="row test">
+            <div class="img-container col-lg-3">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>
+            <div class="text tex col-lg">
+                <h1>Un bon repas ça se partage !</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+
+    <main>
+
+</div>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+
+      <!-- Ajout de mon fichier jQueryUI -->
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+
+    <!-- Ajout de mon fichier de JS -->
+      <script type="text/javascript" src="js/app.js"></script>
+    </body>
+</html>
