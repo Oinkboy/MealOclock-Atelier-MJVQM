6f1fd28d432c9ff25fb6bb5e170cf2060525e71e
Jérémy <jeremy.debski@hotmail.fr>
2018-07-13 12:47:41 +0200

    try version

--- public/header.php
+++ public/header.php
@@ -6,7 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-      <link rel="stylesheet" href="/assets/css/style.css">
+      <link rel="stylesheet" href="assets/css/style.css">
       <link href="https://fonts.googleapis.com/css?family=Miriam+Libre:400,700" rel="stylesheet">
 
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
@@ -15,89 +15,126 @@
     </head>
 
 <body>
-
+    <div class="container-fluid">
     <header>
+            <div id="header-band"></div>
+            <nav class="navbar navbar-expand-md navbar-light justify-content-center">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse justify-content-center">
+                        <ul class="nav nav-pills d-flex">
+                              <li class="nav-item p-3" id="burger-menu">
+                                <span class="navbar-toggler-icon"></span>
+                              </li>
+                              <li class="nav-item flex-fill p-2 form-li">
+                                    <form class="form-inline flex-fill position-relative">
+                                          <i class="fas fa-search position-absolute ml-3"></i>
+                                          <input class="form-control" type="search" placeholder="" aria-label="Search">
+                                    </form>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="h-100" href="#">
+                                          <img src="public/assets/images/E03-logo.svg" alt="">
+                                    </a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#"><i class="fas fa-sign-in-alt "></i> Connexion</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link h-100" href="#"><i class="fas fa-edit"></i> Inscription</a>
+                              </li>
 
-        <nav class="navbar navbar-light bg-light">
+                        </ul>
 
-            <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                  </div>
+            </nav>
+            <nav class="collapse navbar-collapse navbar navbar-expand-md navbar-light justify-content-center" id="navbarTogglerDemo02">
+                  <!-- Cette partie va automatique être masquée en version mobile -->
+                  <div class="" >
+                        <ul class="navbar-nav">
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Communautés</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Evénements</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Membres</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Forum</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">&Agrave; table</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#">Blog</a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
+                              </li>
+                              <li class="nav-item flex-fill p-2">
+                                    <a class="nav-link" href="#"><i class="fab fa-facebook-f"></i></a>
+                              </li>
 
-    <!-- Div contenant Meal'Oclock et link inscription/connexion -->
-            <div class="col-sm-11 navbar navbar-light bg-light">
-                <form class="form-inline my-2 my-lg-0">
-                    <i class="fas fa-search" type="submit"></i>
-                    <input class="form-control mr-sm-2" type="search" placeholder="">
-                </form>
+                        </ul>
+                  </div>
+            </nav>
 
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 flex-row">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">L'image Mealoclock</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link p-2 border border-dark" href="#">Inscription</a>
-                    </li>
-                    <li class="nav-item">
-                        <i class="fas fa-edit"></i>
-                        <a class="nav-link disabled" href="#">Connexion</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- Div du menu burger contenant la deuxième ligne, pas de navbar-expand en classe de la nav pour l'afficher tout le temps -->
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="nav navbar-nav flex-row">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Communautés<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Evénements</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Membres</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Forum</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">À table<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">BLog<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">img Twitter<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">img Fb<span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-            </div>
+      </header>
 
-        </nav>
+   
 
-    </header>
-
-    <main class="container">
-
-    <div class=" bg-light" id="table">
-        <h1>A table</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-
-    <div class="sticky-top bg-secondary" id="test">
-        <row class="">
-            <img src="" alt="">
-            <div class="">
+        <div class="text" id="table">
+            <h1>A table</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    
+    <main class="">
+        <div class="row test">
+            <div class="col-lg-3 img-container">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>   
+            <div class="text col-lg">
                 <h1>Un bon repas ça se partage !</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
-        </row>
-    </div>
+            <div id="img-container" class="col-lg-3">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>
+        </div>
+
+        <div class="row test">
+            <div class="img-container col-lg-3">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>   
+            <div class="text col-lg">
+                <h1>Un bon repas ça se partage !</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-lg-3 img-container">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>
+        </div>
+
+         <div class="row test">
+            <div class="img-container col-lg-3">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>   
+            <div class="text tex col-lg">
+                <h1>Un bon repas ça se partage !</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-lg-3 img-container">
+                <img class="" src="https://picsum.photos/399/255/" alt="">
+            </div>
+        </div>
 
 
+    <main>
 
-    </main>
+</div>
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
